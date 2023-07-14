6c7ec7283b61ab7435ea928e1c6f795d047fd7a1
Jordan Katzen <jkatzen8@gmail.com>
2023-07-14 11:58:42 -0400

    Fix url

--- goodreads_visualizer/url_utils.py
+++ goodreads_visualizer/url_utils.py
@@ -1,9 +1,11 @@
+import re
 from typing import Dict, Union
 from urllib.parse import parse_qs, urlencode, urlparse, urlunparse
 
 
 def format_goodreads_url(url: str, params: Dict[str, Union[str, int]] = {}) -> str:
-    parsed_url = urlparse(url)
+    user_url = _format_user_url(url)
+    parsed_url = urlparse(user_url)
     query_params = parse_qs(parsed_url.query)
     query_params.update(params)
 
@@ -16,12 +18,14 @@ def format_goodreads_url(url: str, params: Dict[str, Union[str, int]] = {}) -> s
     return urlunparse(modified_parts)
 
 
-def parse_url(url: str) -> str:
+def _format_user_url(url: str) -> str:
     parsed_url = urlparse(url)
-    if parsed_url.startswith("/user/show"):
+    if parsed_url.path.startswith("/user/show"):
         user_id = parsed_url.path.split("/")[3]
-    elif parsed_url.startswith("/review/list"):
+    elif parsed_url.path.startswith("/review/list"):
         user_id = parsed_url.path.split("/")[3]
+    elif re.search(r"\d{9}(-\w+)?", url):
+        user_id = re.search(r"\d{9}(-\w+)?", url).group()
     else:
         raise ValueError("Invalid URL")
     return f"https://www.goodreads.com/review/list/{user_id}"
